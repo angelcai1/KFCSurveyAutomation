2d5682a75646c1fdf6d80b0c9c25f7bb6d1064b0
Angel Cai <angelcai1818@gmail.com>
2022-03-30 14:26:32 -0600

    adding KFCSurveyAutomation.py

---
+++ AutoSurveyKFC.py
@@ -0,0 +1,9 @@
+
+def main():
+    # code = "S7200740330222361"
+    # time = "01:06 PM"
+    code = input("Enter KFC Survey Code: ").upper()
+    time = input("Enter Visit Time(Format XX:XX PM/AM): ").upper()
+
+if __name__ == "__main__":
+    main()
